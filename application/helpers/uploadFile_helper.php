df6b2da5d93e39058397ca269e4cf393e8fa190d
Hael12122002 <hendralijaya@gmail.com>
2022-02-08 22:12:21 +0700

    Add a helper functions

---
+++ application/helpers/uploadFile_helper.php
@@ -0,0 +1,25 @@
+<?php 
+
+function uploadFile($filename, $path,$types,$first = true)
+	{
+        $ci = get_instance();
+		$config['upload_path']          = $path;
+		$config['allowed_types']        = $types;
+        $config['max_size']             = 0;
+		if($first){
+			$ci->load->library('upload', $config);
+		}else {
+			$ci->upload->initialize($config);
+		}
+		if ( ! $ci->upload->do_upload($filename))
+        {
+                $error = array('error' => $ci->upload->display_errors());
+				return $error;
+        }
+        else
+        {
+                $upload_data = $ci->upload->data();
+				return $upload_data['file_name'];
+        }
+
+	}
