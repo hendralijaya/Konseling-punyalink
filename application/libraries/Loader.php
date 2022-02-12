186ec1f2be457441977d3fe417c94d037e662aa4
devina36 <ddev7693@gmail.com>
2022-02-12 09:59:26 +0700

    daftar_konselor

---
+++ application/libraries/Loader.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * A simple class which handles loading multiple-databases in codeigniter
+ */
+
+class Loader
+{
+  public function __construct()
+  {
+    $this->load();
+  }
+
+  /**
+   * Load the databases and ignore the old ordinary CI loader which only allows one
+   */
+  public function load()
+  {
+    $CI = &get_instance();
+
+    $CI->db = $CI->load->database('default', TRUE);
+    $CI->db_master = $CI->load->database('master', TRUE);
+  }
+}
---
+++ application/views/home_konseling/v_daftar_konselor.php
@@ -0,0 +1,169 @@
+<main id="daftar_konselor">
+    <div class="container">
+        <div class="form">
+            <h1 class="title text-center">Daftar Menjadi Konselor</h1>
+            <form id="myForm" action="<?= site_url('KonselingController/storeDaftarKonselor') ?>" method="POST">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="steps">
+                            <div class="step active">
+                                1. Informasi Pribadi
+                            </div>
+                            <div class="step">
+                                2. Pendidikan Pengalaman
+                            </div>
+                            <div class="step">
+                                3. Informasi Rekening Bank
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- One "tab" for each step in the form: -->
+                <div class="tab">
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label class="label">Nama</label>
+                            <input id="nama" type="text" name="nama" class="form-control" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Email</label>
+                            <input id="email" type="email" name="email" class="form-control" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Tanggal Lahir</label>
+                            <input id="tanggal_lahir" name="tanggal_lahir" class="form-control datepickerr" placeholder="Check In"  required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Jenis Kelamin</label>
+                            <select id="jenis_kelamin" name="jenis_kelamin" class="form-control selectric" required>
+                                <option value=""><?php echo "Pilih Jenis Kelamin" ?></option>
+                                <option value="Pria">
+                                    <?php echo "Pria" ?? '-' ?></option>
+                                <option value="Wanita">
+                                    <?php echo "Wanita" ?? '-' ?></option>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">No. Handphone</label>
+                            <input id="no_hp" type="number" name="no_hp" class="form-control" value="" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">No. KTP</label>
+                            <input id="no_ktp" type="number" name="no_ktp" class="form-control" value="" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Provinsi</label>
+                            <select id="provinsi" name="provinsi" class="form-control selectric" required>
+                                <option value=""><?php echo "Pilih Provinsi"; ?></option>
+                                <?php foreach ($provinsi as $item) : ?>
+                                <option value="<?php echo $item->id_prov; ?>"><?php echo $item->nama ?? '-'; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Kab/Kota</label>
+                            <select id="kabupaten_kota" name="kabupaten_kota" class="form-control selectric kabupaten_kota" required>
+                                <option value="0"><?php echo "Pilih Kab/Kota" ?></option>
+                                <?php foreach ($kabupaten as $item) : ?>
+                                <option> <?php echo $item->nama ?? '-'; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="form-group col-12">
+                            <label class="label">Alamat</label>
+                            <textarea id="alamat" name="alamat" class="form-control" required></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab">
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label class="label">Gelar Pendidikan S1</label>
+                            <input id="gelar_S1" type="text" name="gelar_S1" class="form-control" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Universitas</label>
+                            <input id="universitas_S1" type="text" name="universitas_S1" class="form-control" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Jurusan</label>
+                            <input id="jurusan_S1" type="text" name="jurusan_S1" class="form-control" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Angkatan</label>
+                            <input id="angkatan_S1" type="email" name="angkatan_S1" class="form-control" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Dari</label>
+                            <input id="dari_S1" type="text" name="dari_S1" class="form-control datepickerr" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Sampai</label>
+                            <input id="sampai_S1" type="text" name="sampai_S1" class="form-control datepickerr" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Gelar Pendidikan S2<span>&nbsp;(bisa dikosongkan bila belum ada)</span></label>
+                            <input id="gelar_S2" type="text" name="gelar_S2" class="form-control"></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Universitas</label>
+                            <input id="universitas_S2" type="text" name="universitas_S2" class="form-control"></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Jurusan</label>
+                            <input id="jurusan_S2" type="text" name="jurusan_S2" class="form-control"></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Angkatan</label>
+                            <input id="angkatan_S2" type="email" name="angkatan_S2" class="form-control"></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Dari</label>
+                            <input id="dari_S2" type="text" name="dari_S2" class="form-control datepickerr"></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Sampai</label>
+                            <input id="sampai_S2" type="text" name="sampai_S2" class="form-control datepickerr"></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Pekerjaan Saat ini</label>
+                            <input id="pekerjaan" type="text" name="pekerjaan" class="form-control"></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Organisasi</label>
+                            <input id="organisasi" type="text" name="organisasi" class="form-control"></input>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab">
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label class="label">Nama Bank</label>
+                            <input id="nama_bank" type="text" name="nama_bank" class="form-control" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Alamat Cabang Bank</label>
+                            <input id="alamat_cabang" type="text" name="alamat_cabang" class="form-control" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Nomor Rekening</label>
+                            <input id="nomor_rekening" type="number" name="nomor_rekening" class="form-control" required></input>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="label">Nama Pemilik Rekening</label>
+                            <input id="nama_rekening" type="text" name="nama_rekening" class="form-control" required></input>
+                        </div>
+                    </div>
+                </div>
+                <div style="overflow: auto">
+                    <div style="float: right">
+                    <button type="button" class="previous">Previous</button>
+                    <button type="button" class="next">Next</button>
+                    <button type="button" class="submit">Submit</button>
+                    </div>
+                </div>
+                <!-- Circles which indicates the steps of the form: -->
+            </form>
+        </div>
+    </div>
+</main>
