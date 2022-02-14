bac16223b4634cfd5efa83fff818dfbedc00d970
Hael12122002 <hendralijaya@gmail.com>
2022-02-14 10:33:35 +0700

    Validation DaftarKonselor

--- application/controllers/KonselingController.php
+++ application/controllers/KonselingController.php
@@ -19,8 +19,44 @@ class KonselingController extends CI_Controller {
 		$this->load->view('home_konseling/partials/js.php',$data);
 	}
 
-	public function daftarKonseling()
+	public function daftarKonselor()
 	{
+		// Informasi Pribadi Konselor
+		$this->form_validation->set_rules('nama', 'Nama', 'required|min_length[5]|max_length[100]');
+		$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[infformasi_pribadi_konselor.email]|max_length[100]');
+		$this->form_validation->set_rules('tanggal_lahir', 'Tanggal Lahir', 'required');
+		$this->form_validation->set_rules('jenis_kelamin', 'Jenis Kelamin', 'required');
+		$this->form_validation->set_rules('no_hp','No HP', 'required|trim|numeric');
+		$this->form_validation->set_rules('no_ktp', 'No KTP', 'required|trim|numeric');
+		$this->form_validation->set_rules('provinsi', 'Provinsi', 'required|alpha|trim');
+		$this->form_validation->set_rules('kabupaten_kota', 'Kabupaten/Kota', 'required|alpha|trim');
+		$this->form_validation->set_rules('alamat', 'Alamat', 'required');
+		// End Informasi Pribadi Konselor
+
+		// Informasi Pendidikan Pengalaman
+		$this->form_validation->set_rules('gelar_S1', 'Gelar S1', 'required|alpha_numeric_spaces');
+		$this->form_validation->set_rules('universitas_S1', 'Universitas S1', 'required|alpha_numeric_spaces');
+		$this->form_validation->set_rules('jurusan_S1', 'Jurusan S1', 'required|alpha_numeric_spaces');
+		$this->form_validation->set_rules('angkatan_S1', 'Angkatan S1', 'required|numeric');
+		$this->form_validation->set_rules('dari_S1', 'Dari S1', 'required|numeric');
+		$this->form_validation->set_rules('sampai_S1', 'Sampai S1', 'required|numeric');
+		$this->form_validation->set_rules('gelar_S2', 'Gelar S2', 'required|alpha_numeric_spaces');
+		$this->form_validation->set_rules('universitas_S2', 'Universitas S2', 'required|alpha_numeric_spaces');
+		$this->form_validation->set_rules('jurusan_S2', 'Jurusan S2', 'required|alpha_numeric_spaces');
+		$this->form_validation->set_rules('angkatan_S2', 'Angkatan S2', 'required|numeric');
+		$this->form_validation->set_rules('dari_S2', 'Dari S2', 'required|numeric');
+		$this->form_validation->set_rules('sampai_S2', 'Sampai S2', 'required|numeric');
+		$this->form_validation->set_rules('pekerjaan', 'Pekerjaan', 'required|alpha_spaces');
+		$this->form_validation->set_rules('organisasi', 'Organisasi', 'required|alpha_numeric_spaces');
+		// End Informasi Pendidikan Pengalaman
+
+		// Informasi Rekening Bank
+		$this->form_validation->set_rules('nama_bank', 'Nama Bank', 'required|alpha_spaces');
+		$this->form_validation->set_rules('alamat_cabang', 'Alamat Cabang', 'required|alpha_spaces');
+		$this->form_validation->set_rules('nama_rekening', 'Nama Rekening', 'required|alpha_spaces');
+		$this->form_validation->set_rules('nomor_rekening', 'Nomor Rekening', 'required|numeric');
+		// End Informasi Rekening Bank
+		if($this->form_validation->run() == FALSE) {
 		$this->load->model('Konseling_model','provinsi');
 		$data['provinsi'] = $this->provinsi->getProvinsi();
 
@@ -29,6 +65,51 @@ class KonselingController extends CI_Controller {
 		$this->load->view('home_konseling/v_daftar_konselor', $data);
 		$this->load->view('home_konseling/partials/footer.php');
 		$this->load->view('home_konseling/partials/js.php');
+		}
+		$dataInformasiPribadi = [
+			'nama' => $this->input->post('nama',TRUE),
+			'email' => $this->input->post('email',TRUE),
+			'tanggal_lahir' => $this->input->post('tanggal_lahir',TRUE),
+			'jenis_kelamin' => $this->input->post('jenis_kelamin',TRUE),
+			'no_hp' => $this->input->post('no_hp',TRUE),
+			'no_ktp' => $this->input->post('no_ktp',TRUE),
+			'provinsi' => $this->input->post('provinsi',TRUE),
+			'kabupaten_kota' => $this->input->post('kabupaten_kota',TRUE),
+			'alamat' => $this->input->post('alamat',TRUE),
+		];
+		$dataPendidikanPengalaman = [
+			'gelar_S1' => $this->input->post('gelar_S1',TRUE),
+			'universitas_S1' => $this->input->post('universitas_S1',TRUE),
+			'jurusan_S1' => $this->input->post('jurusan_S1',TRUE),
+			'angkatan_S1' => $this->input->post('angkatan_S1',TRUE),
+			'dari_S1' => $this->input->post('dari_S1',TRUE),
+			'sampai_S1' => $this->input->post('sampai_S1',TRUE),
+			'gelar_S2' => $this->input->post('gelar_S2',TRUE),
+			'universitas_S2' => $this->input->post('universitas_S2',TRUE),
+			'jurusan_S2' => $this->input->post('jurusan_S2',TRUE),
+			'angkatan_S2' => $this->input->post('angkatan_S2',TRUE),
+			'dari_S2' => $this->input->post('dari_S2',TRUE),
+			'sampai_S2' => $this->input->post('sampai_S2',TRUE),
+			'pekerjaan' => $this->input->post('pekerjaan',TRUE),
+			'organisasi' => $this->input->post('organisasi',TRUE),
+		];
+		$dataInformasiRekeningBank = [
+			'nama' => $this->input->post('nama_bank',TRUE),
+			'alamat_cabang' => $this->input->post('alamat_cabang',TRUE),
+			'nama_rekening' => $this->input->post('nama_rekening',TRUE),
+			'nomor_rekening' => $this->input->post('nomor_rekening',TRUE)
+		];
+		$this->load->model('Konselor_model','konselor');
+		$informasiPribadiId = $this->konselor->storeInformasiPribadiKonselor($dataInformasiPribadi);
+		$pendidikanPengalamanId = $this->konselor->storePendidikanPengalaman($dataPendidikanPengalaman);
+		$informasiRekeningBankId = $this->konselor->storeInformasiRekeningBankKonselor($dataInformasiRekeningBank);
+
+		$dataKonselor = [
+			'informasi_pribadi_konselor_id' => $informasiPribadiId,
+			'informasi_rekening_bank_konselor_id' => $informasiRekeningBankId,
+			'pendidikan_pengalaman_konselor_id' => $pendidikanPengalamanId
+		];
+		$this->konselor->storeKonselor($dataKonselor);
 	}
 
 	function get_kab_kota()
@@ -78,60 +159,4 @@ class KonselingController extends CI_Controller {
 		$this->load->model('Client_konseling_model','client_konseling');
 		$this->client_konseling->storeClientKonseling($dataDaftarKonseling);
 	}
-
-	public function daftarKonselor()
-	{
-		$this->load->view('');
-	}
-
-	public function storeDaftarKonselor()
-	{
-		$dataInformasiPribadi = [
-			'nama' => $this->input->post('nama',TRUE),
-			'email' => $this->input->post('email',TRUE),
-			'tanggal_lahir' => $this->input->post('tanggal_lahir',TRUE),
-			'jenis_kelamin' => $this->input->post('jenis_kelamin',TRUE),
-			'no_hp' => $this->input->post('no_hp',TRUE),
-			'no_ktp' => $this->input->post('no_ktp',TRUE),
-			'provinsi' => $this->input->post('provinsi',TRUE),
-			'kabupaten_kota' => $this->input->post('kabupaten_kota',TRUE),
-			'alamat' => $this->input->post('alamat',TRUE),
-		];
-		$dataPendidikanPengalaman = [
-			'gelar_S1' => $this->input->post('gelar_S1',TRUE),
-			'universitas_S1' => $this->input->post('universitas_S1',TRUE),
-			'jurusan_S1' => $this->input->post('jurusan_S1',TRUE),
-			'angkatan_S1' => $this->input->post('angkatan_S1',TRUE),
-			'dari_S1' => $this->input->post('dari_S1',TRUE),
-			'sampai_S1' => $this->input->post('sampai_S1',TRUE),
-			'gelar_S2' => $this->input->post('gelar_S2',TRUE),
-			'universitas_S2' => $this->input->post('universitas_S2',TRUE),
-			'jurusan_S2' => $this->input->post('jurusan_S2',TRUE),
-			'angkatan_S2' => $this->input->post('angkatan_S2',TRUE),
-			'dari_S2' => $this->input->post('dari_S2',TRUE),
-			'sampai_S2' => $this->input->post('sampai_S2',TRUE),
-			'pekerjaan' => $this->input->post('pekerjaan',TRUE),
-			'organisasi' => $this->input->post('organisasi',TRUE),
-		];
-		$dataInformasiRekeningBank = [
-			'nama' => $this->input->post('nama_bank',TRUE),
-			'alamat_cabang' => $this->input->post('alamat_cabang',TRUE),
-			'nama_rekening' => $this->input->post('nama_rekening',TRUE),
-			'nomor_rekening' => $this->input->post('nomor_rekening',TRUE)
-		];
-		var_dump($dataInformasiPribadi , $dataInformasiRekeningBank);
-		die();
-		$this->load->model('Konselor_model','konselor');
-		$informasiPribadiId = $this->konselor->storeInformasiPribadiKonselor($dataInformasiPribadi);
-		$pendidikanPengalamanId = $this->konselor->storePendidikanPengalaman($dataPendidikanPengalaman);
-		$informasiRekeningBankId = $this->konselor->storeInformasiRekeningBankKonselor($dataInformasiRekeningBank);
-
-		$dataKonselor = [
-			'informasi_pribadi_konselor_id' => $informasiPribadiId,
-			'informasi_rekening_bank_konselor_id' => $informasiRekeningBankId,
-			'pendidikan_pengalaman_konselor_id' => $pendidikanPengalamanId
-		];
-		$this->konselor->storeKonselor($dataKonselor);
-	}
-
 }
